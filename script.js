21f86a2d5e0f7ee115f93eddfe0cfe2226292e2e
Rayan Taghizadeh <rayant@berkeley.edu>
2024-06-01 01:44:53 -0700

    website files

---
+++ script.js
@@ -0,0 +1,185 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('linkedin-icon').addEventListener('click', function() {
+        window.open('https://www.linkedin.com/in/rayan-taghizadeh/', '_blank');
+    });
+
+    document.getElementById('github-icon').addEventListener('click', function() {
+        window.open('https://www.github.com/rayantag', '_blank');
+    });
+
+    document.getElementById('chess-icon').addEventListener('click', function() {
+        window.open('https://www.chess.com/member/fibrinogen', '_blank');
+    });
+});
+
+const aboutMeText = "Hi! My name is Rayan, and I study computer science 💻 and chemical biology 🧬 at UC Berkeley. My interests lie within the broad lens of applied deep learning, from drug synthesis to computer vision for autonomous vehicles. At Berkeley, I was a project manager for <a href='https://bd.studentorg.berkeley.edu' target='_blank' class='bold-blue-link'>Big Data at Berkeley</a>, a developer for <a href='https://www.dailycal.org' target='_blank' class='bold-blue-link'>The Daily Californian</a> 🗞️, a member of CS course staff, and a 2x intramural soccer champion ⚽️. In my free time, I play chess ♟️, watch a variety of sports, and attempt to lift weights when I'm not already hurt."
+document.getElementById('about_me_text').innerHTML = aboutMeText;
+
+const p1description = "An exploration into methods of LLM fine-tuning to adapt to various syntactic structures ('Yoda-like', in this example). Hyperparameters included varying LoRA rank, target modules, and joint training. Trained on a Kaggle Yoda dataset and benchmarked with loss function and Perplexity score metrics. Exciting, this endeavor was."
+document.getElementById('p1desc').innerHTML = p1description;
+
+const p2description = "Inspired by Las Vegas. Web interface featuring LLM-derived sports articles, seasonal stat leaders, and input bar for a user to specify a current NBA player. The page then displays the predicted number of points the player will score in their next game, advised by a neural network trained on >750,000 previous games. Still tinkering to beat the odds."
+document.getElementById('p2desc').innerText = p2description;
+
+const p3description = "Premilinary investigation into the effect of <a href='https://arxiv.org/abs/2003.04960' target='_blank' class='bold-blue-link'>curriculum learning</a> on generation of drug-like molecules. Scoring functions included molecular weight and number of hydrogen bonds, among a few others listed in this <a href='https://jcheminf.biomedcentral.com/articles/10.1186/s13321-024-00812-5' target='_blank' class='bold-blue-link'>REINVENT4</a> paper. Schedules were benchmarked against 'expert' (me), notified by my <a href='https://github.com/rayantag/Drug_Discovery_Models' target='_blank' class='bold-blue-link'>previous attempts at molecular generation</a> and past collaboration with Prescient Design. More work should be done to derive actionable results.";
+document.getElementById('p3desc').innerHTML = p3description;
+
+const p4description = "Mock fellowship paper in a molecular biology laboratory to characterize the Kinesin-5 protein. Procedures included molecular cloning, PCR, Bradford assays, Malachite Green assays, Western Blotting, purification, and other standard techniques recorded in a Benchling notebook. Future direction to translate experiments to Eg5, the human analog of Kinesin-5 with research shown to have potential anti-cancer effects when inhibited. Final slide deck presents the conclusions made from important experiments, and the next steps derived from them."
+document.getElementById('p4desc').innerText = p4description;
+
+const p5description = "Program to encrypt and decrypt cryptographic messages akin to the Engima machine used in WW2, via progressive substitution ciphers performed by rotors. Didn't work so well in the 1940s, so I wouldn't put too much faith in it now. <a href='https://inst.eecs.berkeley.edu/~cs61b/sp22/materials/proj/proj1/index.html' target='_blank' class='bold-blue-link'>Assignment description</a>"
+document.getElementById('p5desc').innerHTML = p5description;
+
+const interests_c = "I first picked up a chess piece when I was four years old and haven't put it down since. Throughout my time playing, I have been: <br>■ UC Berkeley Board 1<br> ■ 1x bronze medal 🥉 (tied for 1st) representing team USA at the U-12 world championship <br> ■ 4x junior national champion <br> ■ 6x junior all-American team <br> ■ >100 students taught through camps and lessons <br> ■ >2900 peak rating on chess.com in blitz and bullet (top 200 in the world)"
+const interests_s = "I have been playing soccer since I was five, and my favorite team is Arsenal FC. I also watch and play a variety of other sports somewhat competitively, including basketball (favorite team varies 🙂‍↕️), football (49ers and Ravens), and tennis."
+const interests_m = "Other current interests include cooking, reading, and walking. I've been told I spend too much time on Twitter. Potentially..."
+
+function showTooltip(id) {
+    const tooltipContainer = document.getElementById('tooltip_container');
+    const tooltipLogo = document.getElementById('tooltip_logo');
+    const tooltipDescription = document.getElementById('tooltip_description');
+    const tooltipTitle = document.getElementById('tooltip_title');
+
+    let logoSrc;
+    let description;
+    let title;
+
+    switch(id) {
+        case 'tesla':
+            logoSrc = './assets/tesla.svg';
+            title = 'Engineering Intern, Summer 2024';
+            description = 'Coming soon!';
+            break;
+        case 'rivian':
+            logoSrc = './assets/rivian.svg';
+            title = 'GenAI Software Engineer, Fall 2023 - Spring 2024';
+            description = "The <b>first task</b> was to build a pipeline to construct an external Design Failure Mode and Effect Analysis (DFMEA) database. Using Google's SERP API and the GPT-4 API, this function allowed reliability engineers to query information about EV parts not already included internally, eliminating the need for manual lookup. The <b>second project</b> involved reworking RivChat, the reliability team's interal ChatBot. We engineered a RAG-based agent capable of retrieving relevant information from their internal database via <a href='https://platform.openai.com/docs/guides/function-calling' target='_blank' class='bold-blue-link'>OpenAI's Function Calling</a>, and the generated external database through <a href='https://platform.openai.com/docs/guides/embeddings/use-cases' target='_blank' class='bold-blue-link'>embeddings</a>.";
+            break;
+        case 'bdab':
+            logoSrc = './assets/bigdata.png';
+            title = 'Data Consultant -> Project Manager, Fall 2022 - Winter 2023';
+            description = "As a data consultant for <b>American Eagle</b> team, I helped write an optimization algorithm to minimize shipping costs by placing specific products in 1 of 7 national AE distribution centers. This was achieved through parsing millions of previous orders, placing specific emphasis on geographic biases, and products commonly ordered together. On the <b>Hologic</b> team, we implemented <a href='https://arxiv.org/abs/2103.03230' target='_blank' class='bold-blue-link'> Barlow Twins</a> on black and white images to generate dense breast cancer image embeddings, which were then used downstream to classify various characteristics. As a project manager for <b>Quokka Brew</b>, I led a team in gathering substantial information on market competition, UI/UX design, and product recommendations.";
+            break;
+        case 'gene':
+            logoSrc = './assets/genentech.svg';
+            title = 'SMPC Data Science Intern, Summer 2022';
+            description = 'Only intern on the Small Molecule Process Chemistry (SMPC) team. Created an ETL Pipeline using SQLAlchemy to construct a relational database with over 700,000 reaction molecules to facilitate machine learning predictions; evaluated multi-objective optimization algorithms against ligands for catalytic reactions, while generating descriptive PowerBI and Tableau visualizations. The database was later applied towards publications by R&D scientists.';
+            break;
+    }
+
+    tooltipLogo.innerHTML = `<img src="${logoSrc}" alt="${id} logo">`;
+    tooltipDescription.innerHTML = description;
+    tooltipTitle.textContent = title;
+    tooltipContainer.style.visibility = 'visible';
+    tooltipContainer.dataset.activeId = id;
+
+    // Highlight the hovered image
+    document.querySelectorAll('.exp_images').forEach(img => img.classList.remove('hover-highlighted'));
+    document.getElementById(id).classList.add('hover-highlighted');
+}
+
+function hideTooltip() {
+    const tooltipContainer = document.getElementById('tooltip_container');
+    if (!tooltipContainer.dataset.keepVisible) {
+        tooltipContainer.style.visibility = 'hidden';
+        tooltipContainer.dataset.activeId = '';
+        document.querySelectorAll('.exp_images').forEach(img => img.classList.remove('hover-highlighted'));
+    }
+}
+
+function toggleTooltip(id) {
+    const tooltipContainer = document.getElementById('tooltip_container');
+    if (tooltipContainer.style.visibility === 'visible' && tooltipContainer.dataset.activeId === id) {
+        tooltipContainer.style.visibility = 'hidden';
+        tooltipContainer.dataset.activeId = '';
+        tooltipContainer.dataset.keepVisible = false;
+        document.querySelectorAll('.exp_images').forEach(img => img.classList.remove('highlighted'));
+    } else {
+        showTooltip(id);
+        tooltipContainer.dataset.keepVisible = true;
+        document.getElementById(id).classList.add('highlighted');
+    }
+}
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    showDesc('chess', document.querySelector('.menu_button'));
+});
+
+function toggleTooltip(id) {
+    const tooltipContainer = document.getElementById('tooltip_container');
+    if (tooltipContainer.style.visibility === 'visible' && tooltipContainer.dataset.activeId === id) {
+        tooltipContainer.style.visibility = 'hidden';
+        tooltipContainer.dataset.activeId = '';
+    } else {
+        showTooltip(id);
+        tooltipContainer.dataset.activeId = id;
+    }
+}
+
+
+function showDesc(name, event) {
+    let description;
+    switch(name) {
+        case 'chess':
+            description = interests_c;
+            break;
+        case 'sports':
+            description = interests_s;
+            break;
+        case 'misc':
+            description = interests_m;
+            break;
+    }
+    document.getElementById('i_desc_par').innerHTML = description;
+
+    // Remove active class from all buttons
+    var buttons = document.querySelectorAll('.menu_button');
+    buttons.forEach(function(button) {
+        button.classList.remove('active');
+    });
+
+    // Add active class to the clicked button
+    event.classList.add('active');
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Function to handle intersection events
+    function handleIntersection(entries, observer) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target); // Stop observing once the element is visible
+            }
+        });
+    }
+
+    // Create an Intersection Observer
+    const observer = new IntersectionObserver(handleIntersection, {
+        threshold: 0.1 // Trigger the event when 10% of the element is visible
+    });
+
+    // Observe each section
+    const sections = document.querySelectorAll('section');
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+
+    const projects = document.querySelectorAll('.project');
+    projects.forEach(project => {
+        observer.observe(project);
+    });
+});
+
+
+
